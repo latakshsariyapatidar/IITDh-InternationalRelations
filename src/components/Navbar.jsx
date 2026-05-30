12b38bda16902c162cb11022203be89a269acc3c
latakshsariyapatidar <latakshpatidar@gmail.com>
2026-05-30 16:10:27 +0530

    Fixed navigation links not getting imported bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 // import LanguageSelector from './LanguageSelector'
-import NavigationLinks from '../data/navigationLinks'
+import NavigationLinks from '../data/NavigationLinks'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
